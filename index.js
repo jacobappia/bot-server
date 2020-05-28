d4ad47cf076e699c52f9a2ef226b36e84f880699
jacobappia <jacob.appia@yahoo.com>
2020-05-28 01:26:15 -0400

    Initial setup.

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ app.post('/', function(req, res) {
 })
 
 // Finally, start our server
-app.listen(3000, function() {
-  console.log('Telegram app listening on port 3000!')
+const port = process.env.PORT || 3000
+app.listen(port, function() {
+  console.log('Telegram app listening on port '+port)
 })
 
